b7615e138fb8f23e0e507c68f09e03374928116e
JIRC78 <jirc2@outlook.com>
2024-03-14 09:59:40 -0500

    cambio jsdocopciones

--- Parcial2/JsDoc Opciones/src/index.js
+++ Parcial2/JsDoc Opciones/src/index.js
@@ -1,13 +1,15 @@
-const axios = require('axios')
-
+/**
+ * Saluda a un usuario.
+ * @param {string} name - El nombre del usuario.
+ * @returns {string} El saludo para el usuario.
+ */
 const greet = name => 'hola ' + name;
 
+/**
+ * Obtiene datos de usuarios desde una API.
+ * @returns {Promise<Object[]>} Un array de objetos que representan a los usuarios.
+ */
 const users = async () => {
     const res = await axios.get('https://jsonplaceholder.typicode.com/users?_limit=2')
     return res.data;
-}
-
-module.exports = {
-    users,
-    greet
 }
